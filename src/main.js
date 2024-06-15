08d48bd5629e47dace66a2fee8bcce0956495e32
Juan Carlos <jccuadradogracia@gmail.com>
2024-06-15 16:49:21 +0200

    first setup application frontend

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import './assets/main.css'
-
+// Pacakges
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// Local files
 import App from './App.vue'
 import router from './router'
 
+// Initializations
 const app = createApp(App)
 
+// Middlewares
 app.use(createPinia())
 app.use(router)
 
+// App is mounted
 app.mount('#app')
