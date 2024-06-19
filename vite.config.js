0e00df7e0e5d7cbe76241d7eac415b177e09e036
Juan Carlos <jccuadradogracia@gmail.com>
2024-06-19 10:22:05 +0200

    gh-pages installed

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Url base
+  base: '/tripshot_vue_frontend/',
   plugins: [vue()],
   resolve: {
     alias: {
